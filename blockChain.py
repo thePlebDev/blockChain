2a26c49ba8bb17b4899cf4f0cac87c6cdfc0725f
Tristan Elliott <tristanelliott7@gmail.com>
2020-11-10 01:34:21 -0400

    adding new block method

--- blockChain.py
+++ blockChain.py
@@ -1,11 +1,33 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        #creates the genesis block.
+        self.new_block(previous_hash=1,proof = 100)
 
-    def new_block(self):
-        # Creates a new Block and adds it to the chain
-        pass
+    def new_block(self,proof,previous_hash=None):
+        """
+            create a new Block in the Blockchain
+            :param proof:<int> the proof given by the Proof of Work algorithm
+            :param previous_hash:(Optional) <str> Hash of previous Block
+            :return <dict> New Block
+        """
+        block ={
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions':self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        #reset the current list of transactions
+        self.current_transactions =[]
+        self.chain.append(block)
+        return block
 
     def new_transaction(self,sender,recipient,amount):
         """
@@ -28,4 +50,4 @@ class Blockchain(object):
     @property
     def last_block(self):
         # Returns the last Block in the chain
-        pass
+        return self.chain[-1];
