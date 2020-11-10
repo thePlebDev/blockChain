f3868503622ef99223307b87aaa3d77fc46aff00
Tristan Elliott <tristanelliott7@gmail.com>
2020-11-10 00:42:40 -0400

    adding transaction function

--- blockChain.py
+++ blockChain.py
@@ -2,14 +2,30 @@ class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
-        def new_block(self):
-            # Creates a new Block and adds it to the chain pass
-            return
 
-        def new_transaction(self):
-            # Adds a new transaction to the list of transactions pass 
-            return
+    def new_block(self):
+        # Creates a new Block and adds it to the chain
+        pass
 
+    def new_transaction(self,sender,recipient,amount):
+        """
+        this will create a new transaction that will go into the next mined Block
+        and it will return the index of the Block that will hold this transaction
 
-        @staticmethod
-        def hash(block): # Hashes a Block pass @property def last_block(self): # Returns the last Block in the chain pass
+        """
+        self.current_transactions.append({
+            'sender':sender,
+            'recipient':recipient,
+            'amount':amount
+        })
+        return self.last_block['index'] + 1
+
+    @staticmethod
+    def hash(block):
+        # Hashes a Block
+        pass
+
+    @property
+    def last_block(self):
+        # Returns the last Block in the chain
+        pass
