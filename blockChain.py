6f48bd8289f638b2af4640f37ff4ea705b0fd143
Tristan Elliott <tristanelliott7@gmail.com>
2020-11-11 18:31:26 -0400

    adding the flask api

--- blockChain.py
+++ blockChain.py
@@ -1,6 +1,9 @@
 import hashlib
 import json
 from time import time
+from uuid import uuid4
+
+from flask import Flask
 
 class Blockchain(object):
     def __init__(self):
@@ -84,3 +87,32 @@ class Blockchain(object):
         guess = f'{last_proof}{proof}'.encode()
         guesss_hash = hashlib.sha256(guess).hexdigest()
         return guesss_hash[:4] =="0000"
+
+
+#INSTANTIATE the node:
+app = Flask(__name__)
+
+#Generate a globally unique adress for this node
+node_identifier = str(uuid4()).replace('-','')
+
+#Instantiate the Blockchain
+blockchain = Blockchain()
+
+@app.route('/mine',methods=['GET'])
+def mine():
+    return "well mine a new block";
+
+@app.route('/transactions/new',methods=['POST'])
+def new_transaction():
+    return "We'll add a new transaction"
+
+@app.route('/chain',methods=['GET'])
+def full_chain():
+    response ={
+        'chain':blockchain.chain,
+        'length':len(blockchain.chain),
+    }
+    return jsonify(response),200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
